044e30928fdeebe093ea1eb5f5d4b881cfa1485e
Marcos Pacheco <marcoatlmadrid23@gmail.com>
2022-10-16 22:33:07 -0300

    Add log to exception of global exception handler

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,26 +1,10 @@
-from flask import jsonify, request
+from flask import jsonify, request, current_app
 
 from .constants import ResponseMessages
 
 
-def resource_not_found(_):
-    """
-    Default return not found endpoint
-    """
-    return jsonify({
-        "message": "Resource not found.",
-        "path": request.full_path
-    }), 404
-
-
-def internal_server_error(_):
-    """
-    Handle with server's errors
-    """
-    return jsonify({"message": ResponseMessages.UNEXPECTED_ERROR}), 500
-
-
 class APIException(Exception):
+
     def __init__(self, message, status_code=400, detail=None, payload=None):
         super().__init__(message)
         self.message = message
@@ -47,17 +31,29 @@ class ValidationException(Exception):
         self.errors = errors
 
 
+def resource_not_found(_):
+    """
+    Default return not found endpoint
+    """
+    return jsonify({
+        "message": "Resource not found.",
+        "path": request.full_path
+    }), 404
+
+
+def internal_server_error(e):
+    """
+    Handle with server's errors
+    """
+    current_app.logger.error(e)
+    return jsonify({"message": ResponseMessages.UNEXPECTED_ERROR}), 500
+
+
 def api_exception_handler(e):
     """
     Handle api exception
     """
     e_json = e.to_dict()
-    e_json["payload"] = {
-        **(e.payload or {}),
-        **(request.get_json() or {}),
-        **request.args
-    }
-
     return jsonify(e_json), e.status_code
 
 
