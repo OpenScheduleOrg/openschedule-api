e6d5bffd80199648602d996edc4e6cbd2540597f
Marcos Pacheco <marcoatlmadrid23@gmail.com>
2022-12-05 02:38:50 -0300

    Add calendar endpoint to return next x free days

--- app/docs/calendar_specs.py
+++ app/docs/calendar_specs.py
@@ -30,3 +30,87 @@ get_specialties_availables = {
         "403": not_authorized_403,
     }
 }
+
+get_free_days = {
+    **tags, "summary":
+    "Return next days with availables schedules",
+    "description":
+    "Return next x days with availables schedules",
+    "operationId":
+    "get_free_days",
+    "security": [{
+        "BearerAuth": []
+    }],
+    "parameters": [{
+        "name": "clinic_id",
+        "in": "query",
+        "required": True,
+        "schema": {
+            "type": "integer",
+        }
+    }, {
+        "name": "specialty_id",
+        "in": "query",
+        "required": True,
+        "schema": {
+            "type": "integer",
+        }
+    }, {
+        "name": "num_days",
+        "in": "query",
+        "required": False,
+        "schema": {
+            "type": "integer",
+            "default": 10
+        }
+    }, {
+        "name": "start_date",
+        "in": "query",
+        "required": True,
+        "schema": {
+            "type": "string",
+            "format": "date",
+        }
+    }, {
+        "name": "first_day_startime",
+        "in": "query",
+        "required": False,
+        "schema": {
+            "type": "date",
+        }
+    }],
+    "responses": {
+        "200": {
+            "description": "list with result of operation",
+            "content": {
+                "application/json": {
+                    "schema": {
+                        "type":
+                        "array",
+                        "items": {
+                            "oneOf": [{
+                                "type": "string",
+                                "format": "date",
+                                "example": "2022-12-20"
+                            }, {
+                                "type": "string",
+                                "format": "date",
+                                "example": "2022-12-21"
+                            }, {
+                                "type": "string",
+                                "format": "date",
+                                "example": "2022-12-22"
+                            }]
+                        },
+                        "examples": [
+                            "2022-12-20", "2022-12-21", "2022-12-22",
+                            "2022-12-22"
+                        ],
+                    }
+                }
+            }
+        },
+        "401": unauthenticated_401,
+        "403": not_authorized_403,
+    }
+}
--- app/routes/calendar.py
+++ app/routes/calendar.py
@@ -1,21 +1,44 @@
+from datetime import timedelta
+
 from flask import request, jsonify
-from sqlalchemy import desc
+from sqlalchemy import desc, func
 from flasgger import swag_from
 
 from . import bp_api
-from ..models import Appointment, Acting, Patient, Clinic, Specialty, Professional, Schedule, session, select, delete, update
+from ..models import Appointment, Acting, Clinic, Specialty, Professional, Schedule, session, select, delete, update
 from ..exceptions import APIException
 from ..utils import useless_params
 from ..constants import ResponseMessages
-from ..validations import validate_payload
-from ..middlewares import token_required, only_admin
+from ..middlewares import token_required
+from ..validations import Validator, validate_payload
 
 from ..docs import calendar_specs
+
+#  Validators  #
+
+validators = {
+    "clinic_id": Validator("clinic_id").required().number(),
+    "specialty_id": Validator("specialty_id").required().number(),
+    "num_days": Validator("num_days").number(),
+    "first_day_startime": Validator("first_day_startime").number(),
+    "start_date": Validator("start_date").date(),
+}
+
+#  END Validators  #
 
 SPECIALTY_FIELDS = (
     Specialty.id,
     Specialty.description,
 )
+
+# QUERIES #
+
+COUNT_APPOINTMENTS = select(func.count(Appointment.id)).where(
+    Appointment.start_time >= Schedule.start_time,
+    Appointment.start_time < Schedule.end_time,
+    Appointment.acting_id == Acting.id)
+
+# QUERIES #
 
 
 @bp_api.route("/calendar/specialties", methods=["GET"])
@@ -25,8 +48,10 @@ def get_specialties_availables(_):
     """
     Obtain clinic specialties with available schedules
     """
-    params = request.args
-    clinic_id = int(params.get("clinic_id"))
+    params = {**request.args}
+    validate_payload(params, validators, ["clinic_id"])
+
+    clinic_id = params.get("clinic_id")
 
     stmt = select(SPECIALTY_FIELDS).distinct().join(
         Acting, Acting.specialty_id == Specialty.id).join(
@@ -37,3 +62,59 @@ def get_specialties_availables(_):
     specialties = [p._asdict() for p in session.execute(stmt).all()]
 
     return jsonify(specialties), 200
+
+
+@bp_api.route("/calendar/free/days", methods=["GET"])
+@swag_from(calendar_specs.get_free_days)
+@token_required
+def get_free_days(_):
+    """
+    Return next x days with availables schedules
+    """
+    params = {**request.args}
+    validate_payload(params, validators)
+
+    clinic_id = params.get("clinic_id")
+    specialty_id = params.get("specialty_id")
+    num_days = params.get("num_days") or 0
+    start_date = params.get("start_date")
+    first_day_startime = params.get("first_day_startime") or 0
+
+    free_days = []
+
+    stmt = select(Schedule.week_day).distinct().join(
+        Acting, Acting.id == Schedule.acting_id).where(
+            Acting.clinic_id == clinic_id,
+            Acting.specialty_id == specialty_id).order_by(Schedule.week_day)
+
+    week_days = list(session.execute(stmt).scalars())
+    base_stmt = select(Schedule.id).join(
+        Acting, Acting.id == Schedule.acting_id).where(
+            Acting.clinic_id == clinic_id, Acting.specialty_id == specialty_id)
+
+    if start_date.weekday() in week_days:
+        stmt = base_stmt.where(
+            Schedule.week_day == start_date.weekday(),
+            Schedule.start_time > first_day_startime,
+            Schedule.max_visits > COUNT_APPOINTMENTS.where(
+                Appointment.scheduled_day == start_date).scalar_subquery())
+
+        if session.execute(stmt).first():
+            free_days.append(start_date)
+            num_days -= 1
+
+    current_date = start_date
+    while num_days > 0:
+        current_date = current_date + timedelta(days=1)
+        if current_date.weekday() not in week_days:
+            continue
+        stmt = base_stmt.where(
+            Schedule.week_day == current_date.weekday(),
+            Schedule.max_visits > COUNT_APPOINTMENTS.where(
+                Appointment.scheduled_day == current_date).scalar_subquery())
+
+        if session.execute(stmt).first():
+            free_days.append(current_date)
+            num_days -= 1
+
+    return jsonify(free_days), 200
